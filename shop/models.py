4e354fc1a34e2b918ac50469871ea860922c905b
MazwiMagubane <mazwimagubane@gmail.com>
2024-10-03 22:07:43 +0200

    Refactored code

---
+++ shop/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Products(models.Model):
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    address2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    province = models.CharField(max_length=200)
+    postcode = models.CharField(max_length=200)
