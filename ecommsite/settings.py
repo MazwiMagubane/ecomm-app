75a1649e22264812aa11982f3902674ef9ece5e5
MazwiMagubane <mazwimagubane@gmail.com>
2024-10-06 19:48:24 +0200

    HTTP headers added

--- ecommsite/settings.py
+++ ecommsite/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE += ["csp.middleware.CSPMiddleware"]
 
 ROOT_URLCONF = 'ecommsite.urls'
 
@@ -130,7 +131,13 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SECURE_HSTS_SECONDS = 30
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
+
+CSP_STYLE_SRC = ["'self'", "cdn.jsdelivr.net", "code.jquery.com"]
+
+CSP_SCRIPT_SRC = ["'self'", "code.jquery.com"]
